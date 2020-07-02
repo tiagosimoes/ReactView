bb3fa383a20d64a8928c271d74d254b31d2cf9fd
Vlad Kostyukevich <forsteam3010@gmail.com>
2020-07-02 19:59:42 +0300

    Init

---
+++ UIAutomation.Framework/AppSession.cs
@@ -0,0 +1,67 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Windows;
+using System;
+
+namespace UIAutomation.Framework
+{
+    public class AppSession
+    {
+        private const string AppName = "Example";
+        private const int timeout = 20;
+        private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        private const string PathToApp = @"C:\Users\Xiaomi\Documents\OutSystems\WebView\Example.Avalonia\bin\Release\netcoreapp3.1\Example.Avalonia.exe";
+
+        protected static WindowsDriver<WindowsElement> session;
+
+        public static void Setup()
+        {
+            if (session == null)
+            {
+                AppiumOptions startAppOptions = PrepareOptionsForApp(PathToApp);
+                try
+                {
+                    new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), startAppOptions);
+                } catch (Exception)
+                {
+                    // catch any exception after startup
+                }
+
+                AppiumOptions desktopOptions = PrepareOptionsForApp("Root");
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), desktopOptions);
+                session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(timeout);
+                Assert.IsNotNull(session);
+
+                var ApplicationWindow = session.FindElementByName(AppName);
+                ApplicationWindow.Click();
+                var ApplicationSessionHandle = ApplicationWindow.GetAttribute("NativeWindowHandle");
+                ApplicationSessionHandle = (int.Parse(ApplicationSessionHandle)).ToString("x");
+
+                AppiumOptions appOptions = new AppiumOptions();
+                appOptions.AddAdditionalCapability("appTopLevelWindow", ApplicationSessionHandle);
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appOptions);
+
+                session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(timeout);
+            }
+        }
+
+        public static void TearDown()
+        {
+            if (session != null)
+            {
+                session.CloseApp();
+                session.Quit();
+                session = null;
+            }
+        }
+
+        private static AppiumOptions PrepareOptionsForApp(string pathToApp)
+        {
+            AppiumOptions options = new AppiumOptions();
+            options.AddAdditionalCapability("platformName", "Windows");
+            options.AddAdditionalCapability("app", pathToApp);
+            options.AddAdditionalCapability("deviceName", "WindowsPC");
+            return options;
+        }
+    }
+}
---
+++ UIAutomation.Framework/TestScenario.cs
@@ -0,0 +1,28 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+
+namespace UIAutomation.Framework { 
+
+[TestFixture]
+public class TestScenario : AppSession
+{
+        [Test]
+        public void ClickSomething()
+        {
+            var menuItem = session.FindElementByTagName("MenuItem");
+            menuItem.Click();
+        }
+
+        [SetUp]
+        public static void SetUp()
+        {
+            Setup();
+        }
+
+        [TearDown]
+        public static void ClassCleanup()
+        {
+            TearDown();
+        }
+    }
+}

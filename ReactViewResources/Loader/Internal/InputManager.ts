2f371f313600cba8148a4c8fc440582b548b3ba4
Tiago Simões <tiago.simoes@outsystems.com>
2021-10-28 08:31:54 +0100

    removed cursor hack as it is no longer needed

--- ReactViewResources/Loader/Internal/InputManager.ts
+++ ReactViewResources/Loader/Internal/InputManager.ts
@@ -16,8 +16,6 @@ export function disableMouseInteractions() {
         layer.style.top = "0";
         layer.style.left = "0";
         layer.style.zIndex = "2147483646";
-        //TODO TCS ugly workaround while showing progress dialog is not working
-        layer.style.cursor = "wait";
         layer.tabIndex = -1;
     }
 
